cff4dc2f92f92eed2703ad851458c51c820c7667
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2023-08-27 20:13:27 +0500

    Add files via upload

---
+++ Day-13 (23-August-2023)/script.js
@@ -0,0 +1,25 @@
+// X---------------------------------- Some more Array Methods -------------------------------------X
+
+// Extracting String Characters
+// There are three methods of extracting string character in JavaScript . Let's discuss all step by step
+
+// let str = "Owais Nadeem";
+// let newStr = str.charAt();
+// console.log(newStr);
+
+// let str = "Owais Nadeem";
+// let newStr = str.charCodeAt(11); // It prints a unique number for each and every javaScript number from (0 to 65535)
+// console.log(newStr);
+
+// let str = "Owais Nadeem";
+// let newStr = str.charCodeAt(str.length - 1); // To print the last number inside our array // It prints a unique number for each and every javaScript number from (0 to 65535)
+// console.log(newStr);
+
+// JavaScript Access Property in a string:
+
+// let str = "Owais Nadeem";
+// let newStr = str[8]; // It will just print the value at the reauired index
+
+// console.log(newStr);
+
+// X----------------------------------------------------------------------------------------X
