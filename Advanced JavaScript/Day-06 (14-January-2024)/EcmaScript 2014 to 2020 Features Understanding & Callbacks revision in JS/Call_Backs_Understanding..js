b47f4c266973410f439d460f86cda8142005d611
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-01-20 11:17:10 +0500

    Add files via upload

---
+++ Advanced JavaScript/Day-06 (14-January-2024)/EcmaScript 2014 to 2020 Features Understanding & Callbacks revision in JS/Call_Backs_Understanding..js
@@ -0,0 +1,32 @@
+// Higher Order Function:
+// Any Function , which takes another function as an argument is called HOF(Higher Order Function)
+
+// CallBack Function:
+// Any function which has passed as an argument in a higher order function , is called as CallBack Function
+
+// Create a calculator function to understand this concept in a better way.
+
+// let add = (a, b) => {
+//   return a + b;
+// };
+
+// let subtract = (a, b) => {
+//   return a - b;
+// };
+
+// let multiply = (a, b) => {
+//   return a * b;
+// };
+
+// let divide = (a, b) => {
+//   return a / b;
+// };
+
+// let calculator = (a, b, func) => {
+//   return func(a, b);
+// };
+
+// console.log(calculator(18, 9, divide));
+
+// console.log(add(4, 9));
+// console.log(subtract(34, 9));
---
+++ Advanced JavaScript/Day-06 (14-January-2024)/EcmaScript 2014 to 2020 Features Understanding & Callbacks revision in JS/script.js
@@ -0,0 +1,30 @@
+// String Method to add padding in it
+
+// These are the methods which are introduced by exma script 2017 ES8
+
+// ====================================== 1 ======================================
+
+// If you wanna add padding in the start
+// string = "My name is Owais ".padStart(20); // This is the padding (20) which is (string.length-20) -> that's why it is the padding of 3 numbers
+// console.log(string);
+
+// ====================================== 2 ======================================
+// If you wanna add padding in any string in the ending:
+// string = "My name is Owais ".padEnd(46);
+// console.log(string);
+
+// ====================================== 3 ======================================
+
+// Another method is about objects , for example if I want to access and get the values in an object then I can get them without using a loop through following method
+
+// let obj = {
+//   My_name: "owais",
+//   My_Age: 22,
+//   My_Hobby: "Coding",
+// };
+
+// console.log(Object.values(obj)); // thorugh this method I got the values of an object in a very easy way using the object method
+
+// If I want to access both things i.e 'key' and 'values' in an object . Then , I can use the following way:
+
+// console.log(Object.entries(obj)); // Also an Interview Question is this , that It will also convert a single entuity (key + value) into an array and the whole object collectively will be converted into an array
