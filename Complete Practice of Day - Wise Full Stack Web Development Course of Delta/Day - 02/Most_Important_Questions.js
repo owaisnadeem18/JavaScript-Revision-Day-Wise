0d3f84356872b74a5f2350204ec260ff9626d3ca
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-18 13:36:08 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 02/Most_Important_Questions.js
@@ -0,0 +1,28 @@
+// Comaprision operator of ==
+
+// console.log(0 == " "); // It will print true
+
+// console.log(0 == NaN); // It will print false (because typeof Number is 0 )
+
+// console.log(null == undefined); // It will print true
+
+// console.log(0 == false); // It will print true (because the statement is true)
+
+// In programming of JS:
+// 0 -> false
+// 1 -> true
+
+// Comaprision operator of ===
+
+// console.log(0 === " "); // It will print false
+
+// console.log(0 === NaN); // It will print false (because types are same of both of them , but their values are not )
+// console.log(typeof NaN);
+// console.log(typeof 0);
+// // Their types are same , but both of the numbers are not same
+
+// console.log(null === undefined); // It will print false
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// console.log(0 === false); // It will print false (because the statement is false) , their numbers are same , but data types are not
