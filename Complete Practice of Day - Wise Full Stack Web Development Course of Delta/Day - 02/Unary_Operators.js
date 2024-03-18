a0dc614a2cde4b01eff9228608ea940aa1fee76f
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-18 13:17:13 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 02/Unary_Operators.js
@@ -0,0 +1,10 @@
+let a = 7;
+let b = 9;
+
+console.log(a);
+console.log(a++); // pehlay aactual value print karay ga , baad may update karay ga
+console.log(a);
+
+console.log(b++); // print 9 hua hay , magar value update ho kar 10 ho chuki hay backend pay
+
+console.log(++b); // 10 already ho chuki thi value , magar ab ++b k andar 1 plus hogya meray pass 10 may , jawab aya 11
