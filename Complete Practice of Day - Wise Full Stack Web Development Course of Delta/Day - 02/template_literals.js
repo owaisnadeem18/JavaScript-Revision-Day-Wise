b2763099d1646d68a2208cf3e66ffb0a90041e90
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-18 13:16:38 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 02/template_literals.js
@@ -0,0 +1,13 @@
+// In the template literals , you can use backticks of your keyboard and along with this templates you can enclose your calculation part in ${} , it is mostly used in the industries to print and display some result in our console
+
+// `` backticks
+
+let myName = "Owais Nadeem";
+let a = 45;
+let b = 55;
+
+console.log(
+  `My Full Name is = ${myName} and the addition of two numbers ${a} + ${b} = ${
+    a + b
+  }`
+);
