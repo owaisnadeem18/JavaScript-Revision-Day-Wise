1a1fb4fd2c6b2dac534bc440c5efebbe2c6de282
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-19 13:09:31 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 03/Logical_Operators.js
@@ -0,0 +1,21 @@
+// OR , AND & NOT Operator
+
+// AND Operator , will return true , if both of the statements are ture
+
+// OR Operator , will return false , if both of the statements are false , all else conditions will return true
+
+// NOT Operator , will return true , if both of the statements are ture
+
+// AND Operator
+
+marks = 85;
+
+console.log(marks < 90 && marks > 80); // return true because both of the statements were true
+
+// OR Operator
+
+console.log(marks < 60 && marks > 90); // return false because both of the statements were true
+
+// NOT Oeprator
+
+console.log(!true); // will return false , because it reverts true to false and false to true
