d4e925fd2c5f0b9395551db493bd5beaa4958634
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-19 12:53:09 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 03/else.js
@@ -0,0 +1,9 @@
+// If nothing will be true then , else will must run
+
+let age = 90;
+
+if (age < 50) {
+  console.log("Age is smaller than 50");
+} else {
+  console.log("I am else block of conditional statements");
+}
