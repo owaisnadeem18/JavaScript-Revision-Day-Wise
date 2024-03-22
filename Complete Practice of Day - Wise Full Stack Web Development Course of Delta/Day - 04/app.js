e7aadb26f1a8eeeb4acf21533620f8159e05c873
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-22 15:26:31 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 04/app.js
@@ -0,0 +1,23 @@
+// Here we have to discuss about the switch case statements in JavaScript
+
+let a = 9;
+
+switch (a) {
+  case 1:
+    console.log("one");
+    break;
+  case 2:
+    console.log("two");
+    break;
+  case 3:
+    console.log("three");
+    break;
+  case 4:
+    console.log("four");
+    break;
+  case 5:
+    console.log("five");
+    break;
+  default:
+    console.log("I am default case ");
+}
---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 04/js.js
@@ -0,0 +1,50 @@
+// // let a = 6;
+
+// // let b = a++; // ye bolta hay k a ko update kiye bina mjh may daaal do
+
+// // console.log(b);
+
+// // let x = 4;
+
+// // let y = ++x;
+// // console.log(y);
+// // console.log(x);
+
+// // // a++;
+
+// // // console.log(a);
+
+// // // ++a;
+// // // console.log(a);
+
+// // assignment question
+
+// let a = 2;
+// let b = 1;
+
+// // --a - --b + ++b + b--;
+
+// // 1 - 0 + 1 + 0;
+
+// // console.log(--a - --b + ++b + b--);
+
+// let y = 4;
+
+// console.log(y);
+// console.log(--y);
+// console.log(y);
+// console.log(y++);
+// console.log(y);
+// console.log(++y);
+// console.log(y);
+
+// dry run the code
+
+// let a = 2;
+// let b = 1;
+
+// console.log(`--a is = ${--a}`);
+// console.log(`--b is = ${--b}`);
+// console.log(a);
+// console.log(b);
+// console.log(--b);
