a2be76e6b3f54c5e2a7818bc43a9215db7a7ab81
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-26 01:15:38 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 09/further_methods.js
@@ -0,0 +1,11 @@
+// further_methods of strings
+
+// Replace
+
+let str = " My Name is Owais Badami";
+
+console.log(str);
+console.log(str.replace("Badami", "Nadeem")); // replaced string
+
+// repeat method of strings in JS
+console.log(str.repeat(300).length);
