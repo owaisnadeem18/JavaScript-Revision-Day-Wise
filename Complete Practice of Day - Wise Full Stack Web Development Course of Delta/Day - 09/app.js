f8cbdc788dde082d04c4e78ac35823894763f1a2
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-26 01:10:25 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 09/app.js
@@ -0,0 +1,12 @@
+// String chaining method is that we are performing 2 diiferent operations on a string in a same time
+
+let str = "               Owais Nadeem             ";
+
+// console.log(str);
+// console.log(str.trim());
+
+// console.log(str.toUpperCase());
+
+// Now If I want to perform both operations at a time , then:
+
+console.log(str.trim().toUpperCase());
---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 09/slicing_In_JavaScript.js
@@ -0,0 +1,9 @@
+// Slicing In JavaScript:
+
+let str = "Owais Nadeem";
+
+// negative slicing
+
+console.log(str.length);
+
+console.log(str.slice(-0)); // ((entered negative index number) - 1)
