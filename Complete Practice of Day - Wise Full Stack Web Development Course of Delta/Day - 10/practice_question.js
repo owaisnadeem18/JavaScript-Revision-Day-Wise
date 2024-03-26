65c34000c3e54075c06b99d662e1863c0b99e3e9
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-26 19:58:11 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 10/practice_question.js
@@ -0,0 +1,17 @@
+// Let's complete a practice question here:
+
+// Here we have the names of months , we have to replace them with some other months
+
+let arr = ["Jan", "Feb", "March", "april", "May", "June"];
+
+console.log(arr);
+// we have to replace the arr indexes with ["July" , "August" ,"September" , "October" , "November" , "December"]
+
+arr[0] = "July";
+arr[1] = "August";
+arr[2] = "September";
+arr[3] = "October";
+arr[4] = "November";
+arr[5] = "December";
+
+console.log(arr);
