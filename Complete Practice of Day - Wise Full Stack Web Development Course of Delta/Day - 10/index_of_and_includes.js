e5bdff18b98b5bea3972b1a97b37f685ac961e5c
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-26 20:15:42 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 10/index_of_and_includes.js
@@ -0,0 +1,13 @@
+// Now, we have array methods as index of and includes methods
+
+let arr = ["Owais", 456, true, false, "900"];
+
+// IndexOf:
+// It returns the index of something.
+
+console.log(arr.indexOf("owais")); // it will return -1 , because 450 is not even existing in it
+
+// Includes :
+// It search for a value of something.
+
+console.log(arr.includes(456)); // It returns boolean true , if the value exists inside it. Otherwise it returns false
