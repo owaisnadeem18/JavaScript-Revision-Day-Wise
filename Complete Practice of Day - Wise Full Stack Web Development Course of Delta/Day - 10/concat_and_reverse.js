aa6b18ddc453b9a59e7182026fb75ebe80db5769
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-26 20:21:57 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 10/concat_and_reverse.js
@@ -0,0 +1,15 @@
+// here we have to concat two different arrays with each other
+
+arr1 = [98, 43, 23, 13, 49];
+arr2 = [398, 4, 5343, 5223, 43, 92];
+
+console.log(arr1);
+console.log(arr2);
+
+concat_arr = arr1.concat(arr2);
+console.log(concat_arr);
+
+// Here we have to discuss reverse method of array:
+
+console.log(arr1);
+console.log(arr1.reverse()); // here we have reversed our array
