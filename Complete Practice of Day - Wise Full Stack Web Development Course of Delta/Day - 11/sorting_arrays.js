99daf22846d4913ee9c4d2d87d949d94a7402543
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-28 02:46:31 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 11/sorting_arrays.js
@@ -0,0 +1,10 @@
+// Here, we have to sort the arrays. So, we will code it according to it
+
+let arr = [45, 23, 94, 32, 89, 95, 65];
+
+console.log(arr);
+console.log(arr.sort());
+
+arr = ["a", "b", "x", "l ", "r", "q"];
+console.log(arr);
+console.log(arr.sort());
