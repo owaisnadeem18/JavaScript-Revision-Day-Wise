7ae5ffe92af33b091bc6bb5165b7648240a50c4a
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-28 15:32:11 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 11/Question_06.js
@@ -0,0 +1,15 @@
+// Qs6. Write a JavaScript program to check if an element exists in an array or not
+
+let arr = [" 67", 53, " Owais", true];
+let arr2 = [];
+
+if (arr.length == 0) {
+  console.log("The array is empty");
+} else {
+  console.log(`The array is ${arr} and it is non empty`);
+}
+if (arr2.length == 0) {
+  console.log("The array is empty");
+} else {
+  console.log(`The array is ${arr2} and it is non empty`);
+}
