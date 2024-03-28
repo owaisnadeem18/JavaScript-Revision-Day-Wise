0de6787b19b4aa58f98edd46f41e6cd7c50fd221
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-28 15:13:29 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 11/Question_03.js
@@ -0,0 +1,10 @@
+// Qs3. Write a JavaScript program to check whether a string is blank or not.
+
+// let str = prompt("Enter any string: ");
+let str = "kjetkterjgkdrjg";
+
+if (str.length == 0) {
+  console.log("The entered string is empty");
+} else {
+  console.log(`The entered string is '${str}' and it is non-empty `);
+}
