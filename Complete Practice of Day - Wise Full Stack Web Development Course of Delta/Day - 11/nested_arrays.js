935b248d832dc0af70c452a97c9f55857e35f173
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-28 12:08:10 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 11/nested_arrays.js
@@ -0,0 +1,10 @@
+// Nested Arrays
+
+let arr = [
+  [45, 23],
+  [32, 90],
+  [49, 29],
+];
+
+console.log(arr[0]);
+console.log(arr[0][1]); // if the index is not found , then we will see the result as 'undefined'
