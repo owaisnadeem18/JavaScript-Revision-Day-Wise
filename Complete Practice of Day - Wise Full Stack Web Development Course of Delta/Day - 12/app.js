a1daf00870e31d9b7b33321c37318f2f9ea3d79b
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-28 20:01:06 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 12/app.js
@@ -0,0 +1,32 @@
+// Print all odd numbers between 01 to 15 using for loop
+
+// for (let i = 1; i <= 15; i++) {
+//   if (i % 2 != 0) {
+//     console.log(i);
+//   }
+// }
+
+// Print all even numbers between 01 to 10 using for loop
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+// console.log("\n");
+// console.log(" Backwards \n");
+// // backwards loop of for loop
+
+// for (let x = 10; x >= 2; x--) {
+//   if (x % 2 == 0) {
+//     console.log(x);
+//   }
+// }
+
+// Infinite Loop Understanding:
+
+// By default the conditional place of for loop will be true , because we are not writing it
+
+for (let i = 1; i++; ) {
+  console.log(i);
+}
---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 12/multiplication_table.js
@@ -0,0 +1,13 @@
+// let i = 5;
+
+// for (let x = 1; x <= 10; x++) {
+//   console.log(`${5} X ${x} = ${i * x} `);
+// }
+
+// Print the multiplication Table Entered by a user
+
+// let n = +prompt("Enter a number = ");
+
+// for (let i = n; i <= n * 10; i += n) {
+//   console.log(i);
+// }
