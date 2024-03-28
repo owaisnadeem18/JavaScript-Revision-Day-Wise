2c4538dc74ff92cbc13b81eba246329100991d8e
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-28 11:27:41 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 11/array_practice_quest.js
@@ -0,0 +1,34 @@
+// // Write a program in which using splice method of array replace some months of array with some other months
+
+// // original array:
+
+// // arr = ["January", "July", "March", "August"];
+
+// // replace it like this
+
+// // arr = ["July", "June", "March", "August"];
+
+// original_arr = ["January", "July", "March", "August"];
+
+// original_arr.splice(0, 2, "July", " June");
+
+// console.log(original_arr);
+
+// Another Important Question:
+
+// Return the index of JavaScript when the following arr is reversed:
+
+let langauges = [
+  "c",
+  "c++",
+  "sql",
+  "c#",
+  "python",
+  "javaScript",
+  "R programming",
+  "Java",
+];
+
+find_out = langauges.reverse().indexOf("javaScript");
+
+console.log(find_out);
