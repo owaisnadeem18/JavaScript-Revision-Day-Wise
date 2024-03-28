8cd728ef35093b1b273a3b865bb15923971e7090
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-28 12:28:07 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 11/practice_ques_tic_tac_toe.js
@@ -0,0 +1,9 @@
+// There is a practice question of tic tac toe in JavaScript. We have to solve this question using 2D array
+
+let game = [
+  ["X", null, "O"],
+  [null, "X", "O"],
+  ["O", null, "X"],
+];
+
+console.log(game);
