a55fdf6ae18cedb0451663a8cdf853500fcb877d
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-31 01:28:07 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 13/loops_in_arrays.js
@@ -0,0 +1,13 @@
+// Loops in arrays:
+
+let arr_of_cars = ["mehran", "corolla", "honda", "suzki", "wag-nr"];
+
+// for (i = 0; i < arr_of_cars.length; i++) {
+//   console.log(arr_of_cars[i]);
+// }
+
+// If you want to run this array in the reverse order
+
+for (i = arr_of_cars.length - 1; i >= 0; i--) {
+  console.log(arr_of_cars[i]);
+}
