b4aac7fd325e95179e4a2e7296bd89180d7e40b2
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-30 15:17:28 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 13/break_keyword.js
@@ -0,0 +1,8 @@
+// break keword understanding
+
+for (let i = 1; i < 10; i++) {
+  if (i == 5) {
+    break; // we normally use our break keyword with while loop
+  }
+  console.log(i);
+}
---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 13/while_loop.js
@@ -0,0 +1,11 @@
+// We use while loop when we know that we have to deal with some specific condition. It will keep on running till our specific condition is true
+
+let i = 1;
+
+while (i < 21) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+  i++;
+}
+qa;
