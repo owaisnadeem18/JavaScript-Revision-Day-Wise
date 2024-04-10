2c8bf76a7c50730b27dd851d03c623d6c3d39d76
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-11 00:43:28 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 14/for_of_loops_In_Arrays.js
@@ -0,0 +1,13 @@
+// for_of_loops_In_Arrays.
+
+// fruits = ["apple ", "banana", "manngo", "orange"];
+
+// for (i of fruits) {
+//   console.log(i);
+// } // whenever we are required to print the indexes of an array in a single go , without using for loop and conditions , then we can use this way
+
+// There is another way for this thing
+
+for (let i of "OWais Nadeem") {
+  console.log(i);
+}
