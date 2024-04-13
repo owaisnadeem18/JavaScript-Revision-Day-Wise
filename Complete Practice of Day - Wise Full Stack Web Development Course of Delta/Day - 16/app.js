d2e96a627738e0e2163a56baeeac4fe73c625cc4
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-13 23:54:42 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 16/app.js
@@ -0,0 +1,11 @@
+// Object Literals
+
+let obj = {
+  myName: "Owais nadeem",
+  age: 22,
+  profession: "student",
+};
+
+console.log(obj);
+
+// (keys and values) of Objects do not carry the same sequence and order
---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 16/create_a_post.js
@@ -0,0 +1,12 @@
+let post = {
+  userName: "owaisnadeem18",
+  post_content:
+    "hello my name is Owais Nadeem and I am programming in JavaScript ",
+
+  likes: 345,
+  comments: 30,
+};
+
+// Now we can also print this post:
+
+console.log(post);
