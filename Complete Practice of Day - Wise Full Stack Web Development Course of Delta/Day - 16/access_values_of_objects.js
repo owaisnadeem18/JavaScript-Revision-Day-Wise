e926c4add78032b1076bf93e25565a17afeda4fb
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-14 00:00:06 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 16/access_values_of_objects.js
@@ -0,0 +1,20 @@
+let post = {
+  userName: "owaisnadeem18",
+  post_content:
+    "hello my name is Owais Nadeem and I am programming in JavaScript ",
+
+  likes: 345,
+  comments: 30,
+};
+
+console.log(post["userName"]);
+
+// We can also write it like this
+
+console.log(post.userName);
+
+// Now we can also print this post:
+
+console.log(post);
+
+// If we want to print or access variables from objects then we will use square brackets
