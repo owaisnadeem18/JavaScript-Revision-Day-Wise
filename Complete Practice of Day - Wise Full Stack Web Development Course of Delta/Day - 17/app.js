f2fe21d5778d5915ca864b162cbbffb7f1406413
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-16 23:52:51 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 17/app.js
@@ -0,0 +1,20 @@
+// Objects of Objects
+
+let obj = {
+  stud_1: {
+    marks: "89",
+    name: "Owais ",
+  },
+  stud_2: {
+    marks: "97",
+    name: "Hamza ",
+  },
+  stud_3: {
+    marks: "93",
+    name: "Hassan ",
+  },
+};
+
+console.log(obj.stud_1.marks);
+
+// There is an object inside the object
