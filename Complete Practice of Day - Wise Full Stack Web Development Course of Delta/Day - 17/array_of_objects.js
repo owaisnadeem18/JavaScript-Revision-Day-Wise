63cf33b93abc3525d76db0bf99967fe6baa0e701
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-16 23:59:42 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 17/array_of_objects.js
@@ -0,0 +1,32 @@
+// Array of Objects
+
+let obj = [
+  {
+    marks: "89",
+    name: "Owais ",
+  },
+  {
+    marks: "97",
+    name: "Hamza ",
+  },
+  {
+    marks: "93",
+    name: "Hassan ",
+  },
+];
+
+// console.log(obj[1]);
+
+console.log(obj[1].name);
+
+// console.log(obj[2]);
+
+console.log(obj[1].marks);
+
+// We can also add a new key into our objects
+
+obj[1].gender = "Male";
+
+console.log(obj[1]);
+
+// There is an array inside the object
