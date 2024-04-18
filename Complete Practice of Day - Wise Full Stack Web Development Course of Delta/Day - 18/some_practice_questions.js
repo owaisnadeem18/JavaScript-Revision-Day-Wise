04a3004dcb2d288c1f6cc4cd5d8f9879b952afaf
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-18 18:07:23 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 18/some_practice_questions.js
@@ -0,0 +1,25 @@
+// Generate some random numbers between 1 to 100 (must use floor)
+
+// let num = Math.random();
+
+// num = Math.floor(num * 100) + 1; // we are ranging it between 1 to 100
+
+// console.log(num);
+
+// Generate some random numbers between 1 to 5 (must use floor)
+
+// let num = Math.random();
+
+//generate them between 01 to 05
+
+// num = Math.floor(num * 5) + 1;
+
+// console.log(num);
+
+// If you want to generate random numbers just between 41 to 45
+
+let num = Math.random(); //
+
+num = Math.floor(num * 5) + 41;
+
+console.log(num);
