47a2b56cfc725984befb70368d3728791877a724
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-18 17:11:07 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 18/random_integers.js
@@ -0,0 +1,9 @@
+// generate some random integers
+
+let num = Math.random();
+
+num = num * 5; // we are going to generate the numbers between 1 to 5
+
+num = Math.ceil(num); // we are reducing the extra points after the decimal
+
+console.log(num);
