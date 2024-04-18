46c273c2a7f58829830219d3d22f65166ee6aad2
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-18 17:02:40 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Day - 18/app.js
@@ -0,0 +1,13 @@
+// Math functions in JavaScript
+
+console.log(Math.PI); // for the value of Pie
+
+console.log(Math.abs(-45)); // will return the positive value
+
+console.log(Math.floor(-4.0001)); // It will take thje value to small number
+
+console.log(Math.floor(4.9)); // It will take the value to small number
+
+console.log(Math.ceil(-3.4)); // It will take the value to greator number
+
+console.log(Math.random()); // it will give the value between 0 to 1 , but the value will never be 0 nor 1
