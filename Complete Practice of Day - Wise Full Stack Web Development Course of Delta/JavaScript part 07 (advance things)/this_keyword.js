d8d73540026eb1973583252928b59dda2233d883
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-30 11:25:14 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 07 (advance things)/this_keyword.js
@@ -0,0 +1,22 @@
+// This keyWord in JavaScript.
+
+// Any object which is executing any method in itself, is our this function in JavaScript
+
+// let obj = {
+//   myName: "Owais Nadeem",
+//   myAge: 23,
+//   eng: 30,
+//   maths: 9,
+//   phy: 5,
+//   getAvg() {
+//     console.log(this);
+//     let avg = this.eng + this.maths + this.phy;
+//     console.log(avg);
+//   },
+// };
+
+// function check_This() {
+//   console.log(this);
+// }
+
+// check_This();
