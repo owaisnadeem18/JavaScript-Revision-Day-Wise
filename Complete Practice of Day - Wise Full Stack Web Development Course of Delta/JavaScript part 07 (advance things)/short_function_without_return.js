2e4012ae02b57c2df098a2649d10fcfdb1d674d7
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-04-30 12:52:00 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 07 (advance things)/short_function_without_return.js
@@ -0,0 +1,10 @@
+// Short hand method of writing a function , without using return keyword in it
+
+// write a very short function of multiplying two numbers
+
+// There are four different numbers and we wanna multipy them with each others
+
+// This is a way to write a function without using return keyword
+let Short_Func_Mult = (a, b, c, d) => a * b * c * d;
+
+console.log(Short_Func_Mult(2, 3, 2, 1));
