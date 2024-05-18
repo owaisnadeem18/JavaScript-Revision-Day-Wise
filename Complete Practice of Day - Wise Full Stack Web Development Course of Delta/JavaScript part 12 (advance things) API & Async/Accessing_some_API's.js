1d23ed9aa4d87f745b181cddaeeef999997aac73
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-05-18 09:44:38 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 12 (advance things) API & Async/Accessing_some_API's.js
@@ -0,0 +1,5 @@
+// https://dog.ceo/api/breeds/image/random (Here, we have to access this kind of very first APi which is related to dog images)
+
+// https://www.boredapi.com/api/activity (This is another API which can be used to show you about any activity when you are feeling a little bit bored)
+
+// https://catfact.ninja/fact (This is an API of Cats , where you can see the any single fact about cats after refreshing it)
