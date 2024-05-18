399eaeef1d4b7b0dd5b2751d8363ec1a034d110a
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-05-18 10:24:24 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 12 (advance things) API & Async/What_is_JSON.js
@@ -0,0 +1,9 @@
+// JSON Stands for JavaScript Object Notation
+
+// We get our data of API in the form of JSON format
+
+// To verify the result of our JSON file , we can visit JSON validator
+
+// visit www.json.org for more information about JSON
+
+// https://catfact.ninja/fact
