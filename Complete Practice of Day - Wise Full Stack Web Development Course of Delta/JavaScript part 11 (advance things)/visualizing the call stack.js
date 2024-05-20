a576aefaef3d4fb7467d365797653581b4870c4a
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-05-20 17:37:08 +0500

    Add files via upload

--- Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 11 (advance things)/visualizing the call stack.js
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 11 (advance things)/visualizing the call stack.js
@@ -1,3 +1,19 @@
 // visualizing the call stack
 
 // Here, we have to visualize the call stack means we just have to add functions with each other by calling one function into another
+
+let function_one = () => {
+  return 1;
+};
+
+let function_two = () => {
+  return 2 + function_one();
+};
+let function_three = () => {
+  let add = function_one() + function_two();
+  return add;
+};
+
+console.log(function_three());
+
+// stack frame in programming is the name of all this process
