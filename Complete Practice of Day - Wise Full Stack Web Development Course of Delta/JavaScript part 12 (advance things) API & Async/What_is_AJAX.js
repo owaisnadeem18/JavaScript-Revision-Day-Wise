0509a941e7fb89b51f4ce63ac30e1a692aef1127
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-05-20 12:12:49 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 12 (advance things) API & Async/What_is_AJAX.js
@@ -0,0 +1,3 @@
+// AJAX stands for Asynchronous javaScript XML
+
+// It is used to take the requests of asynchronous JavaScript and show the data in XML format instead of JSON , because it was an old way of returning API data to user
