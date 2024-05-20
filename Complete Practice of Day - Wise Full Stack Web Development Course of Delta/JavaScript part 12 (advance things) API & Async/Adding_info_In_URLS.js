6eea47e00aac0dcb54776bb98bbe1c6d9675bb91
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-05-20 13:09:22 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 12 (advance things) API & Async/Adding_info_In_URLS.js
@@ -0,0 +1,12 @@
+// Here we have to add some information in our URL's , that's why we have to find out any way
+
+// q = (stands for a) query string in our URL
+
+// https://www.google.com/search?q=Dr+Tahir+Ul+Qari+Emotional+Byanat
+
+// We can also replace our id of API , with a valid request
+
+// http://universities.hipolabs.com/search?name=Pakistan
+// this above mentioned API is an API of our universities of the world i.e http://universities.hipolabs.com , where we can add additional information to get the search results of our desired country
+
+// In the query section of API URL , if you are making any mistake , then it will not give an error of 404 not found , because in the search result , it ignores the result which is invalid
