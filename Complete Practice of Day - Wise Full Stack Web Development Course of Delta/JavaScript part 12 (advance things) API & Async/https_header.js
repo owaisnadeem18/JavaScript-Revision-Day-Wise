c31db8b63a35bfab92c40bdcc38cb9aa2c6086ee
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-05-20 13:08:25 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 12 (advance things) API & Async/https_header.js
@@ -0,0 +1,5 @@
+// Here we have studied about https headers
+
+// meta data is "data" about "data"
+
+// Here we have also studied that how we can convert a http header from default to (text/plain) or (application/json)
