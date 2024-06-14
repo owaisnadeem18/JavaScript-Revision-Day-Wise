c64032931f560dac37f371c9d740775914f5e796
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-14 21:02:23 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript part 07 (advance things)/_this_.js
@@ -0,0 +1,23 @@
+// simple function k andar 'this' key word ka matlab hota hay us puray object ki (key value pairs) jo is function k bahar bnay huay hongay
+
+// let obj = {
+//   a: 30,
+//   b: 70,
+//   c: 100,
+//   sum() {
+//     console.log(this); // ye this khud k andar object key tamam key values ko carry karta hay
+
+//     let sum = this.a + this.b + this.c;
+//     console.log(sum);
+//   },
+// };
+
+// obj.sum();
+
+// function aik_fun() {
+//   console.log(this);
+// }
+
+// aik_fun();
+
+// console.log(this); // bhalay tum is (this) naami function ko JS key single line may print karwao ya aik function may (simple function) may print karwao , ye hamesha aapko return karay ga aik *****WINDOW***** object
