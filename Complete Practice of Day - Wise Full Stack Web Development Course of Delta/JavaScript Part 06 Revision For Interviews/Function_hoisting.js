b789e5fc6b9354b39bd93f40a8c56dc1b22f148b
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-14 19:58:48 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript Part 06 Revision For Interviews/Function_hoisting.js
@@ -0,0 +1,21 @@
+// Simple Functions in JavaScript follow this hoisting method in JS , but fat arrow functions are unable to work in such scenario
+
+// sayHello(); // "Hello, World!"
+
+// function sayHello() {
+//   console.log("Hello, World!");
+// }
+
+// Arrow functions:
+
+// greet(); // it will not work in this scenario , JavaScript could not hoist these fat arrow functions
+
+// let greet = () => {
+//   console.log("Hello !!!");
+// };
+
+// let greet = () => {
+//   console.log("Hello !!!");
+// };
+
+// greet();
