a443bcdb0a7edcc9298a79894da66a6f66f7fb91
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-14 19:26:12 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/JavaScript Part 06 Revision For Interviews/variable_hoisting.js
@@ -0,0 +1,16 @@
+// console.log(a);
+
+// var a = 34;
+// console.log(a);
+
+// console.log(a);
+
+// let a = 34;
+// console.log(a);
+
+// console.log(a);
+
+// const a = 34;
+// console.log(a);
+
+// You can not access the variable before declaring and initializing it , this is  the concept of variable hoisting
