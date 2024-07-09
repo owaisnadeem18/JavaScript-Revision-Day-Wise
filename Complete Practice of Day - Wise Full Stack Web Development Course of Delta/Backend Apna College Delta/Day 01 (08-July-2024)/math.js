4000461c51db9affcde9083a874c9913f2c4e31e
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-07-09 13:04:41 +0500

    Add files via upload

---
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Backend Apna College Delta/Day 01 (08-July-2024)/math.js
@@ -0,0 +1,4 @@
+export const sum = (a, b) => a + b;
+export const multiply = (a, b) => a * b;
+export const subtr = (a, b) => a - b;
+export const PI = 3.142;
--- Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Backend Apna College Delta/Day 01 (08-July-2024)/script.js
+++ Complete Practice of Day - Wise Full Stack Web Development Course of Delta/Backend Apna College Delta/Day 01 (08-July-2024)/script.js
@@ -8,15 +8,26 @@
 //   console.log(` Hello ! ${argvs[i]}`);
 // }
 
-const sum = (a, b) => a + b;
+// const sum = (a, b) => a + b;
 
-const multiply = (a, b) => a * b;
+// const multiply = (a, b) => a * b;
 
-let obj = {
-  sum: sum,
-  mult: multiply,
-  a: 34,
-  b: 16,
-};
+// let obj = {
+//   sum: sum,
+//   mult: multiply,
+//   a: 34,
+//   b: 16,
+// };
 
-module.exports = obj;
+// module.exports = obj;
+
+// ---------- What if we want to use Import in the place of require ------------
+
+import { sum } from "./math.js";
+// import { generate } from "random-words";
+
+import { PI } from "./math.js";
+
+console.log(sum(3, 5));
+console.log(PI);
+// console.log(generate());
